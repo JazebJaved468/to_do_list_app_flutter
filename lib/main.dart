e0cbfc53e59571155dbbe977f844bd11b200549a
Jazeb_Javed <jazebjaved52@gmail.com>
2023-10-07 22:41:42 +0500

    firebase structure added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:to_do_list/firebase_options.dart';
 import 'screens/task_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -12,9 +16,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: "To DO List",
+      title: "To Do List",
       debugShowCheckedModeBanner: false,
-      home:  ListApp(),
+      home: ListApp(),
     );
   }
 }
